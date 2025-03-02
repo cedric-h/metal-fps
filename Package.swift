f2563addc1a9b5e5cfccadaa9439ae662d2f719f
Cedric Hutchings <cedric@passivelogic.com>
2025-03-02 13:04:12 -0700

    change name to fps

--- Package.swift
+++ Package.swift
@@ -4,11 +4,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "wallTap",
+    name: "fps",
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "wallTap"),
+        .executableTarget(name: "fps"),
     ]
 )
---
+++ README.md
@@ -0,0 +1,3 @@
+opens a window with a first person camera strolling around a building
+
+recommended: `git ls-files | entr -r swift run fps`
